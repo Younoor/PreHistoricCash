5e988cb7d80e7272fbde462640faf5a88b6ca36e
Yousef Noor <yous3fnoor@gmail.com>
2024-02-25 10:46:32 -0600

    Maybe another keyboard

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,24 +1,29 @@
-import FontAwesome from '@expo/vector-icons/FontAwesome';
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { useEffect, useState } from 'react';
-import MyKeyboard from '@/src/components/MyKeyboard';
-import { myColors } from '@/src/styles/Colors';
+import FontAwesome from "@expo/vector-icons/FontAwesome";
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from "@react-navigation/native";
+import { useFonts } from "expo-font";
+import { Stack } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect, useState } from "react";
+import MyKeyboard from "@/src/components/MyKeyboard";
+import { myColors } from "@/src/styles/Colors";
 
-import { useColorScheme } from '@/components/useColorScheme';
-import { SafeAreaView, StyleSheet, Switch, Text } from 'react-native';
-import TabLayout from './(tabs)/_layout';
+import { useColorScheme } from "@/components/useColorScheme";
+import { SafeAreaView, StyleSheet, Switch, Text } from "react-native";
+import TabLayout from "./(tabs)/_layout";
+import NumericKeyboard from "@/components/NumericKeyboard";
 
 export {
   // Catch any errors thrown by the Layout component.
   ErrorBoundary,
-} from 'expo-router';
+} from "expo-router";
 
 export const unstable_settings = {
   // Ensure that reloading on `/modal` keeps a back button present.
-  initialRouteName: '(tabs)',
+  initialRouteName: "(tabs)",
 };
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
@@ -26,7 +31,7 @@ SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
   const [loaded, error] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+    SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
     ...FontAwesome.font,
   });
 
@@ -49,18 +54,32 @@ export default function RootLayout() {
 }
 
 function RootLayoutNav() {
+  const [amount, setAmount] = useState<string>("");
   const colorScheme = useColorScheme();
-  const [theme, setTheme] = useState('light');
+  const [theme, setTheme] = useState("light");
+  const handleKeyPress = (input: string) => {
+    console.log("Input:", input);
+    // You can handle the input value here, e.g., update state or perform further processing
+    setAmount(input);
+  };
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      
-      <SafeAreaView style={theme === 'dark' ? styles.container : [styles.container, {backgroundColor: 'black'}]}>
+    <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
+      <SafeAreaView
+        style={
+          theme === "dark"
+            ? styles.container
+            : [styles.container, { backgroundColor: "black" }]
+        }
+      >
         <Switch
-          value={theme === 'dark'}
-          onValueChange={() => setTheme(theme === 'light' ? 'dark' : 'light')}
+          value={theme === "dark"}
+          onValueChange={() => setTheme(theme === "light" ? "dark" : "light")}
         />
         <MyKeyboard />
+        {/* <NumericKeyboard style={
+          styles.middle
+        } onKeyPress={handleKeyPress} /> */}
         <TabLayout />
       </SafeAreaView>
     </ThemeProvider>
@@ -71,10 +90,13 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: myColors.light,
-    alignItems: 'center',
-    justifyContent: 'flex-start',
+    alignItems: "center",
+    justifyContent: "flex-start",
+  },
+  middle: {
+    flex: 1,
+    justifyContent: "center",
   },
 });
-
 
 //        <Stack.Screen name="modal" options={{ presentation: 'modal' }} />
